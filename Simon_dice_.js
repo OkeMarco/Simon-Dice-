13b40bd977e047ac61522a08d683024450fe57ad
OkeMarco <okemarco41@gmail.com>
2026-04-08 13:29:08 +0200

    Mejoras y correciones de la ultima version

--- Simon_dice_.js
+++ Simon_dice_.js
@@ -1,70 +1,49 @@
 const tColores = {
-  ROJO:   0,
-  VERDE:  1,
-  AZUL:   2,
-  DORADO: 3,
+    ROJO:   0,
+    VERDE:  1,
+    AZUL:   2,
+    DORADO: 3,
 }
-const MAX_COLORES_SEQ= 12
-//const secuenciaColores= new Array(MAX_COLORES_SEQ);
+const MAX_COLORES_SEQ = 12;
 
-function charToColor(color){
-  
-    switch(color.toLowerCase()){
-        case "r":
-             return tColores.ROJO
-        case "v":
-            return tColores.VERDE
-        case "a":
-            return tColores.AZUL
-        case "d":
-            return tColores.DORADO 
-        default: 
-            return null                   
+function charToColor(color) {
+    switch (color.toLowerCase()) {
+        case "r": return tColores.ROJO;
+        case "v": return tColores.VERDE;
+        case "a": return tColores.AZUL;
+        case "d": return tColores.DORADO;
+        default:  return null;
     }
-    function intToColor(numero){
-    switch(numero){
-        case 0:
-             return tColores.ROJO
-        case 1:
-            return tColores.VERDE
-        case 2:
-            return tColores.AZUL
-        case 3:
-            return tColores.DORADO
-        default: 
-            return null                   
+}   
+
+function intToColor(numero) {
+    switch (numero) {
+        case 0: return tColores.ROJO;
+        case 1: return tColores.VERDE;
+        case 2: return tColores.AZUL;
+        case 3: return tColores.DORADO;
+        default: return null;
     }
 }
-function generarSecuencia ( numColores ){
-   const secuenciaColores=[]
-   for(let i=0; i<MAX_COLORES_SEQ;i++){
-    const aleatorio = Math.floor(Math.random() * (numColores + 1));
-     const color = intToColor(aleatorio);
-    secuenciaColores.push(color)
 
-
-   }
-   return secuenciaColores
-}
-}
-function mostrarColor(color){
-    switch(color){
-        case rojo: rojo==tColores.ROJO
-                return "rojo"
-        case verde: verde==tColores.VERDE
-                return "verde"
-        case azul: azul==tColores.AZUL
-                return "azul"
-        case dorado: dorado==tColores.DORADO
-                return "dorado"                            
-
+function generarSecuencia(numColores) {
+    const secuenciaColores = [];
+    for (let i = 0; i < MAX_COLORES_SEQ; i++) {
+        const aleatorio = Math.floor(Math.random() * (numColores + 1));
+        secuenciaColores.push(intToColor(aleatorio));
     }
-   }
+    return secuenciaColores;
+}
 
-function comprobarColor(secuenciaColores,indice,color){
-    if(secuenciaColores[indice]==color){
-        return true
-    }else{
-        return false
+function mostrarColor(color) {
+    switch (color) {
+        case tColores.ROJO:   return "Rojo";
+        case tColores.VERDE:  return "Verde";
+        case tColores.AZUL:   return "Azul";
+        case tColores.DORADO: return "Dorado";
     }
+}
+
+function comprobarColor(secuenciaColores, indice, color) {
+    return secuenciaColores[indice] === color;  
 }
